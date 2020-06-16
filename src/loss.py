f38099c139057b5067ce0df46084023b284c0f81
renesat <s-paradox@ro.ru>
2020-06-16 15:48:56 +0300

    Fix loss expt

--- src/loss.py
+++ src/loss.py
@@ -24,10 +24,10 @@ def bdt_loss(pred_y_batch,
         pred_y_norm = torch.norm(pred_y, 2)
         pred_noise_norm = torch.norm(pred_noise, 2)
 
-        prod_y = torch.sqrt(torch.sum((pred_y - y) ^ 2))
+        prod_y = torch.sqrt(torch.sum((pred_y - y)**2))
         sdr_y_loss = prod_y / y_norm / pred_y_norm
 
-        prod_noise = torch.sqrt(torch.sum((pred_noise - noise) ^ 2))
+        prod_noise = torch.sqrt(torch.sum((pred_noise - noise)**2))
         sdr_noise_loss = prod_noise / noise_norm / pred_noise_norm
 
         alpha = y_norm**2 / (noise_norm**2 + y_norm**2)
